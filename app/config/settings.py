ad286f840bee092086aabbfd9ef533103c7763bd
chrisaor <pchpch0070@gmail.com>
2018-06-23 12:38:23 +0900

    login view구현 및 static폴더 설정

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,9 +14,10 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,3 +128,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
--- app/members/views.py
+++ app/members/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            print(request.user.is_authenticated)
+            return redirect('index')
+        else:
+            return redirect('login')
+
+
+
+    return render(request, 'members/login.html')
+
+def signup(request):
+    return render(request, 'members/signup.html')
